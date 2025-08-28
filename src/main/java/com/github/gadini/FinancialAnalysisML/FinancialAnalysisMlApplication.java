2833267aa13d96efeb7c7943426b48839308ee79
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-08-28 16:17:42 -0300

    first commit

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/FinancialAnalysisMlApplication.java
@@ -0,0 +1,13 @@
+package com.github.gadini.FinancialAnalysisML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAnalysisMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAnalysisMlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gadini/FinancialAnalysisML/FinancialAnalysisMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gadini.FinancialAnalysisML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAnalysisMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

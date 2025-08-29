def888c7e09b0df7f07693e3416ca00b4767de34
gadini <alvesrochagadini@gmail.com>
2025-08-28 22:07:30 -0300

    Ajuste empresa mapper import nao utilizado

--- src/main/java/com/github/gadini/FinancialAnalysisML/domain/mapper/EmpresaMapper.java
+++ src/main/java/com/github/gadini/FinancialAnalysisML/domain/mapper/EmpresaMapper.java
@@ -3,7 +3,6 @@ package com.github.gadini.FinancialAnalysisML.domain.mapper;
 import com.github.gadini.FinancialAnalysisML.domain.response.EmpresaResponse;
 import com.github.gadini.FinancialAnalysisML.persistence.entity.Empresa;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EmpresaMapper {

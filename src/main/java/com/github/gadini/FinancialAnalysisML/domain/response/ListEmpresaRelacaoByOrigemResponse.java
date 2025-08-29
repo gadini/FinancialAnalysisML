434eef8cb7b0e385b5642c40463e493c1b1a51d7
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-08-29 10:59:13 -0300

    Adicionando response listEmpresaRelacaoByOrigem em EmpresaRelacao

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/domain/response/ListEmpresaRelacaoByOrigemResponse.java
@@ -0,0 +1,22 @@
+package com.github.gadini.FinancialAnalysisML.domain.response;
+
+import com.github.gadini.FinancialAnalysisML.domain.enums.StatusEnum;
+import com.github.gadini.FinancialAnalysisML.domain.enums.TipoRelacaoEnum;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListEmpresaRelacaoByOrigemResponse {
+
+    private Long id;
+    private String empresaDestinoNome;
+    private TipoRelacaoEnum tipoRelacao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private StatusEnum status;
+}

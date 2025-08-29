5f6eb0fce0a082c9bd7549f44df38190630ca8b8
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-08-29 10:44:08 -0300

    Adicionando Controller EmpresaRelacao com endpoint de listar todas as relacoes entre empresas

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/controller/EmpresaRelacaoController.java
@@ -0,0 +1,25 @@
+package com.github.gadini.FinancialAnalysisML.controller;
+
+import com.github.gadini.FinancialAnalysisML.domain.response.ListAllEmpresaRelacaoResponse;
+import com.github.gadini.FinancialAnalysisML.service.EmpresaRelacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("relacoes")
+public class EmpresaRelacaoController {
+
+    @Autowired
+    private EmpresaRelacaoService empresaRelacaoService;
+
+    @GetMapping
+    public ResponseEntity<PagedModel<ListAllEmpresaRelacaoResponse>> list(Pageable page) {
+        return ResponseEntity.ok(empresaRelacaoService.listarRelacoesEmpresas(page));
+    }
+
+}

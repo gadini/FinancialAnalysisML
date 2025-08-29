a550596c159bd923613c7ece4b0c26293e1c77e6
gadini <alvesrochagadini@gmail.com>
2025-08-28 22:09:02 -0300

    Adicionando Transacao Service com seu primeiro metodo de listar transacoes

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/service/TransacaoService.java
@@ -0,0 +1,23 @@
+package com.github.gadini.FinancialAnalysisML.service;
+
+import com.github.gadini.FinancialAnalysisML.domain.mapper.TransacaoMapper;
+import com.github.gadini.FinancialAnalysisML.domain.response.TransacaoResponse;
+import com.github.gadini.FinancialAnalysisML.persistence.repository.TransacaoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransacaoService {
+
+    @Autowired
+    TransacaoMapper transacaoMapper;
+
+    @Autowired
+    TransacaoRepository transacaoRepository;
+
+    public Page<TransacaoResponse> listarTransacoes(Pageable pageable){
+        return transacaoRepository.findAll(pageable).map(transacaoMapper::toResponse);
+    }
+}

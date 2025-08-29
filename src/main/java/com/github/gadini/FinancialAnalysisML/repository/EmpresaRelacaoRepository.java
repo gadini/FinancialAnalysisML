abe60113e96140a35b9a4d1d920d95784ee1c9ee
gustavo.gadini <gustavo.gadini@redeflex.com.br>
2025-08-29 10:42:19 -0300

    Adicionando Repository EmpresaRelacao

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/repository/EmpresaRelacaoRepository.java
@@ -0,0 +1,7 @@
+package com.github.gadini.FinancialAnalysisML.repository;
+
+import com.github.gadini.FinancialAnalysisML.domain.entity.EmpresaRelacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRelacaoRepository extends JpaRepository<EmpresaRelacao, Long> {
+}

a99bb0f975e93430d06f93d4661a648fc7882b90
gadini <alvesrochagadini@gmail.com>
2025-09-01 18:03:39 -0300

    Ajuste Classificacao

--- src/main/java/com/github/gadini/FinancialAnalysisML/domain/dto/ClassificacaoDto.java
+++ src/main/java/com/github/gadini/FinancialAnalysisML/domain/dto/ClassificacaoDto.java
@@ -3,7 +3,6 @@ package com.github.gadini.FinancialAnalysisML.domain.dto;
 import com.github.gadini.FinancialAnalysisML.domain.enums.MomentoEnum;
 import lombok.Data;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Data
@@ -11,6 +10,5 @@ public class ClassificacaoDto {
 
     private Long empresaId;
     private MomentoEnum momento;
-    private BigDecimal scoreAnalitico;
     private LocalDate dataAnalise;
 }
--- src/main/java/com/github/gadini/FinancialAnalysisML/domain/entity/Classificacao.java
+++ src/main/java/com/github/gadini/FinancialAnalysisML/domain/entity/Classificacao.java
@@ -5,16 +5,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Getter
@@ -31,9 +27,6 @@ public class Classificacao extends BaseEntity{
     @Enumerated(EnumType.STRING)
     @Column(name = "momento", nullable = false)
     private MomentoEnum momento;
-
-    @Column(name = "score_analitico", precision = 5, scale = 2)
-    private BigDecimal scoreAnalitico;
 
     @Column(name = "data_analise", nullable = false)
     private LocalDate dataAnalise;

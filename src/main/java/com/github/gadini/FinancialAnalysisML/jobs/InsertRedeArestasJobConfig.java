2e140778d64fd439dd5706e79879727b8f846558
gadini <alvesrochagadini@gmail.com>
2025-09-27 19:17:02 -0300

    Adicionando Rede Arestas Job Config

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/jobs/InsertRedeArestasJobConfig.java
@@ -0,0 +1,40 @@
+package com.github.gadini.FinancialAnalysisML.jobs;
+
+import com.github.gadini.FinancialAnalysisML.tasklets.InsertRedeArestasTasklet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@RequiredArgsConstructor
+public class InsertRedeArestasJobConfig {
+
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    private final InsertRedeArestasTasklet tasklet;
+
+    @Bean
+    public Job insertRedeArestasJob() {
+        Step step = insertRedeArestasStep();
+        return new JobBuilder("insertRedeArestasJob", jobRepository)
+                .incrementer(new RunIdIncrementer())
+                .preventRestart()
+                .start(step)
+                .build();
+    }
+
+    @Bean
+    public Step insertRedeArestasStep() {
+        return new StepBuilder("insertRedeArestasStep", jobRepository)
+                .tasklet(tasklet, transactionManager)
+                .build();
+    }
+
+}

d421bc4da0e47c16539b09a7d9a4e5596c3e70ec
gadini <alvesrochagadini@gmail.com>
2025-09-27 19:12:31 -0300

    Adicionando Rede Arestas Tasklet

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/tasklets/InsertRedeArestasTasklet.java
@@ -0,0 +1,29 @@
+package com.github.gadini.FinancialAnalysisML.tasklets;
+
+import com.github.gadini.FinancialAnalysisML.service.RedeArestasService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+
+@Slf4j
+@Component
+public class InsertRedeArestasTasklet implements Tasklet {
+
+    @Autowired
+    private RedeArestasService redeArestasService;
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        LocalDate inicio =  LocalDate.now().minusMonths(1).withDayOfMonth(1);
+        LocalDate fim = inicio.withDayOfMonth(inicio.lengthOfMonth());
+        redeArestasService.criarRedeArestas(inicio, fim);
+        log.info("DEU CERTO");
+        return RepeatStatus.FINISHED;
+    }
+}

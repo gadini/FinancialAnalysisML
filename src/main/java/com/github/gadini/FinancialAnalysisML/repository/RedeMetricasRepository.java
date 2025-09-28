2113cb9cdd9f95536bbec6a7ad80e604227f8976
gadini <alvesrochagadini@gmail.com>
2025-09-28 00:13:34 -0300

    Adicionando repository rede metricas

---
+++ src/main/java/com/github/gadini/FinancialAnalysisML/repository/RedeMetricasRepository.java
@@ -0,0 +1,7 @@
+package com.github.gadini.FinancialAnalysisML.repository;
+
+import com.github.gadini.FinancialAnalysisML.domain.entity.RedeMetricas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RedeMetricasRepository extends JpaRepository<RedeMetricas, Long> {
+}

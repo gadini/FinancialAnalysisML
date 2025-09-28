d7ce72ec6d250e10f4df99f2ba450d52bc70bb16
gadini <alvesrochagadini@gmail.com>
2025-09-28 00:14:10 -0300

    Ajustando classificacao repository e rede arestas repository

--- src/main/java/com/github/gadini/FinancialAnalysisML/repository/ClassificacaoRepository.java
+++ src/main/java/com/github/gadini/FinancialAnalysisML/repository/ClassificacaoRepository.java
@@ -2,6 +2,25 @@ package com.github.gadini.FinancialAnalysisML.repository;
 
 import com.github.gadini.FinancialAnalysisML.domain.entity.Classificacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ClassificacaoRepository extends JpaRepository<Classificacao, Long> {
+
+    interface IdExternoMomento {
+        String getIdExterno();
+        String getMomento();
+    }
+
+    @Query(value = """
+        SELECT e.id_externo AS idExterno, c.momento AS momento
+          FROM classificacao c
+          JOIN empresa e ON e.id = c.empresa_id
+         WHERE c.data_analise = :data
+        """, nativeQuery = true)
+    List<IdExternoMomento> findIdExternoAndMomentoByData(@Param("data") LocalDate data);
+
 }
--- src/main/java/com/github/gadini/FinancialAnalysisML/repository/RedeArestasRepository.java
+++ src/main/java/com/github/gadini/FinancialAnalysisML/repository/RedeArestasRepository.java
@@ -5,8 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public interface RedeArestasRepository extends JpaRepository<RedeArestas, Long> {
+
+    List<RedeArestas> findByPeriodoInicioAndPeriodoFim(LocalDate periodo_inicio, LocalDate periodo_fim);
 
     @Procedure(procedureName = "sp_criar_rede_arestas")
     void spCriarRedeArestas(LocalDate p_inicio, LocalDate p_fim);
